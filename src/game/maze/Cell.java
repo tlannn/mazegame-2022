9eea4c1f7e0dddf80edb3b8463082475a1f042b7
Everaere <emma.everaere2.etu@univ-lille.fr>
2022-01-29 21:16:09 +0100

    Add constructeur and setter at cell

--- src/game/maze/Cell.java
+++ src/game/maze/Cell.java
@@ -1,5 +1,7 @@
 package game.maze;
 
+import java.util.ArrayList;
+
 /**a cell in the maze*/
 public class Cell{
     private int x;
@@ -7,40 +9,79 @@ public class Cell{
     private boolean northWall;
     private boolean southWall;
     private boolean eastWall;
-    private List <Character> characters;
+    private boolean westWall;
+    private ArrayList <Character> characters;
 
-
+    /*a cell must have its 4 walls */
     public Cell(int x, int y){
-
+    	this.x=x;
+    	this.y=y;
+    	this.northWall=true;
+    	this.southWall=true;
+    	this.eastWall=true;
+    	this.westWall=true;
+    	this.characters= new ArrayList<Character>();
     }
 
     /** tell if there is a north wall to the cell
     @return true if there is a north wall
     */
-    public hasNorthWall(){
+    public boolean hasNorthWall(){
         return this.northWall;
     }
 
     /** tell if there is a south wall to the cell
     @return true if there is a south wall
     */
-    public hasSouthWall(){
+    public boolean hasSouthWall(){
         return this.southWall;
     }
 
     /** tell if there is a east wall to the cell
     @return true if there is a east wall
     */
-    public hasEastWall(){
+    public boolean hasEastWall(){
         return this.eastWall;
     }
 
     /** tell if there is a west wall to the cell
     @return true if there is a west wall
     */
-    public hasWestWall(){
+    public boolean hasWestWall(){
         return this.westWall;
     }
+    
+    /**can add or delete the northWall
+     * 
+     * @param: boolean true if there is a wall 
+     * */
+	public void setNorthWall(boolean northWall) {
+		this.northWall = northWall;
+	}
+
+    /**can add or delete the southWall
+     * 
+     * @param: boolean true if there is a wall 
+     * */
+	public void setSouthWall(boolean southWall) {
+		this.southWall = southWall;
+	}
+	
+    /**can add or delete the eastWall
+     * 
+     * @param: boolean true if there is a wall 
+     * */
+	public void setEastWall(boolean eastWall) {
+		this.eastWall = eastWall;
+	}
+
+    /**can add or delete the westWall
+     * 
+     * @param: boolean true if there is a wall 
+     * */
+	public void setWestWall(boolean westWall) {
+		this.westWall = westWall;
+	}
 }
 
 
