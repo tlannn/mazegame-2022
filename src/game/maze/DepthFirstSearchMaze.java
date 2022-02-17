b174ab69e6410d1f1ff261c32821c7930cb6b2df
Everaere <emma.everaere2.etu@univ-lille.fr>
2022-02-17 19:05:26 +0100

    Add generate but i don't if it works because wrong import, so oskour Theo

--- src/game/maze/DepthFirstSearchMaze.java
+++ src/game/maze/DepthFirstSearchMaze.java
@@ -1,23 +1,26 @@
 package game.maze;
 
-import game.util.Random;
+import java.util.Random;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 //on fait une pile qui retient le chemin de mur déjà visité
 public class DepthFirstSearchMaze extends Maze {
 
-    private Map <Cell, Boolean> AreVisited;
+    private Map <Cell, Boolean> alreadyVisited;
 
     public DepthFirstSearchMaze(int length, int height){
         super(length, height);
-        this.AreVisited = new HashMap <Cell, Boolean>();
+        this.alreadyVisited = new HashMap <Cell, Boolean>();
         for(int i = 0; i < length; i++ ){
             for(int j = 0; j < height; j++){
-              AreVisited.put(this.cells[i][j], false);
+              alreadyVisited.put(this.cells[i][j], false);
             }
         }
     }
@@ -34,40 +37,60 @@ public class DepthFirstSearchMaze extends Maze {
         //la cellule du dessus n'a pas déjà eté visité
         //Cell cellFrontOf=this.cells[cell.getX()-1][cell.getY()];
         Cell cellFrontOf=this.getCell(cell.getX(),cell.getY()-1);
-        if (! AreVisited.get(cellFrontOf)){
+        if (! alreadyVisited.get(cellFrontOf)){
             neighboring.add(cellFrontOf);
         }
       }
       if (cell.getY()!=this.height-1){//si la cellule est pas en bas, on ajoute l'index du dessous
         Cell cellFrontOf=this.getCell(cell.getX(),cell.getY()+1);
-        if (! AreVisited.get(cellFrontOf)){
+        if (! alreadyVisited.get(cellFrontOf)){
             neighboring.add(cellFrontOf);
           }
       }
 
       if (cell.getX()!=0){//si la cellule est pas à gauche du tableau, on ajoute la cellule de gauche
         Cell cellFrontOf=this.getCell(cell.getX()-1,cell.getY());
-        if (! AreVisited.get(cellFrontOf)){
+        if (! alreadyVisited.get(cellFrontOf)){
             neighboring.add(cellFrontOf);
           }
       }
 
       if (cell.getX()!=this.length-1){//si la cellule est pas tout à droite du tableau, on ajoute la cellule de droite
         Cell cellFrontOf=this.getCell(cell.getX()+1,cell.getY());
-        if (! AreVisited.get(cellFrontOf)){
+        if (! alreadyVisited.get(cellFrontOf)){
             neighboring.add(cellFrontOf);
           }
       }
     return neighboring;
     }
 
-
-
     protected void generate(){
       int x = new Random().nextInt(this.length);
       int y = new Random().nextInt(this.height);
-      Cell actualCell= this.getCell(x,y)
+      //je choisit aléatoirement la première cellule
+      Cell actualCell= this.getCell(x,y);
 
+      //intialisation
+      Deque <Cell> areVisited = new ArrayDeque<Cell>();
+      List<Cell> neighboring = new ArrayList <Cell> ();
+
+
+
+      //tant que je n'ai pas visité tout le labyrinthe
+      do{
+      neighboring = this.neighboringCells(actualCell);
+      if (! neighboring.isEmpty()){
+        Cell neighborCell = neighboring.get(neighboring.size());
+        this.removeWall(actualCell, neighborCell);
+        areVisited.push(actualCell);
+        alreadyVisited.put(actualCell, true);
+        actualCell= neighborCell;
+      }
+      else{
+        alreadyVisited.pop();
+        actualCell= alreadyVisited.peek(); //au dernire tour on regardera une liste vide donc actualCell vaudra null
+      }
+    }while (! alreadyVisited.empty());
 
 
     //on crée une hashmap avec comme clé la cellule et comme valeur un boolean qui nous dit si elle a déjà été visité ou non
@@ -80,5 +103,6 @@ public class DepthFirstSearchMaze extends Maze {
     //on detruit le mur entre les 2
 
     //si aucune cellule possible on remonte à la dernière cellule observé (dans notre magnifique pile toute bien faites)
+    //On le fait TANT QUE la pile n'est pas vide
     }
 }
