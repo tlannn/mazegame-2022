e0c164a4ac79907f3e77efd68f9ad1296f8ce116
Everaere <emma.everaere2.etu@univ-lille.fr>
2022-02-17 17:43:15 +0100

    Add end of neighboringCells

--- src/game/maze/DepthFirstSearchMaze.java
+++ src/game/maze/DepthFirstSearchMaze.java
@@ -3,13 +3,23 @@ package game.maze;
 import game.util.Random;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 //on fait une pile qui retient le chemin de mur déjà visité
 public class DepthFirstSearchMaze extends Maze {
-    
+
+    private Map <Cell, Boolean> AreVisited;
 
     public DepthFirstSearchMaze(int length, int height){
         super(length, height);
+        this.AreVisited = new HashMap <Cell, Boolean>();
+        for(int i = 0; i < length; i++ ){
+            for(int j = 0; j < height; j++){
+              AreVisited.put(this.cells[i][j], false);
+            }
+        }
     }
 
     /**
@@ -17,43 +27,48 @@ public class DepthFirstSearchMaze extends Maze {
     *@param cell : the cell we want the neighboring cells
     *@return the neighboring cells of the cell "cell" which are not already visited
     */
+
     private List<Cell> neighboringCells(Cell cell){
       List<Cell> neighboring = new ArrayList <Cell> ();
       if (cell.getY()!=0){//si la cellule est pas en haut, on ajoute l'index du dessus
         //la cellule du dessus n'a pas déjà eté visité
         //Cell cellFrontOf=this.cells[cell.getX()-1][cell.getY()];
         Cell cellFrontOf=this.getCell(cell.getX(),cell.getY()-1);
-        if (! cellFrontOf.getAlreadyVisited() ){
+        if (! AreVisited.get(cellFrontOf)){
             neighboring.add(cellFrontOf);
         }
       }
-      if (cell.gety()!=this.height-1){//si la cellule est pas en bas, on ajoute l'index du dessous
-      Cell cellFrontOf=this.getCell(cell.getX(),cell.getY()+1);
-        if(){//est pas déjà visité dans la hashmap
-          neighboring.add(cellFrontOf);
-        }
+      if (cell.getY()!=this.height-1){//si la cellule est pas en bas, on ajoute l'index du dessous
+        Cell cellFrontOf=this.getCell(cell.getX(),cell.getY()+1);
+        if (! AreVisited.get(cellFrontOf)){
+            neighboring.add(cellFrontOf);
+          }
       }
 
-      if (cell.getx()!=0){//si la cellule est pas à gauche du tableau, on ajoute la cellule de gauche
-      Cell cellFrontOf=this.getCell(cell.getX()-1,cell.getY());
-        if(){//est pas déjà visité dans la hashmap
-          neighboring.add(cellFrontOf);
-        }
+      if (cell.getX()!=0){//si la cellule est pas à gauche du tableau, on ajoute la cellule de gauche
+        Cell cellFrontOf=this.getCell(cell.getX()-1,cell.getY());
+        if (! AreVisited.get(cellFrontOf)){
+            neighboring.add(cellFrontOf);
+          }
       }
 
-      if (cell.getx()!=this.length-1){//si la cellule est pas tout à droite du tableau, on ajoute la cellule de droite
-      Cell cellFrontOf=this.getCell(cell.getX()+1,cell.getY());
-        if(){//est pas déjà visité dans la hashmap
-          neighboring.add(cellFrontOf);
-        }
+      if (cell.getX()!=this.length-1){//si la cellule est pas tout à droite du tableau, on ajoute la cellule de droite
+        Cell cellFrontOf=this.getCell(cell.getX()+1,cell.getY());
+        if (! AreVisited.get(cellFrontOf)){
+            neighboring.add(cellFrontOf);
+          }
       }
     return neighboring;
     }
 
 
 
-
     protected void generate(){
+      int x = new Random().nextInt(this.length);
+      int y = new Random().nextInt(this.height);
+      Cell actualCell= this.getCell(x,y)
+
+
 
     //on crée une hashmap avec comme clé la cellule et comme valeur un boolean qui nous dit si elle a déjà été visité ou non
     //on créer une liste de cellule possible
