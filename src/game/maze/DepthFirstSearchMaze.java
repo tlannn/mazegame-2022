8abb2876db478eb4d629177e7919755885150020
Theo Lannoy <theo.lannoy2.etu@univ-lille.fr>
2022-02-20 23:54:26 +0100

    Correct typo and complete documentation

--- src/game/maze/DepthFirstSearchMaze.java
+++ src/game/maze/DepthFirstSearchMaze.java
@@ -7,101 +7,123 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 
-//on fait une pile qui retient le chemin de mur déjà visité
+/**
+ * Class DepthFirstSearchMaze that represents a maze in the game, generated using the randomized depth-first search algorithm.
+ * The algorithm is defined like this :
+ * 	- start from a random cell
+ * 	- then repeat this process :
+ * 		- look around for the neighboring cells that are not yet visited, and choose and random cell among these
+ * 		- open the wall between the cell and the neighbor
+ * 		- add this cell to a stack to keep track of visited cells
+ * 	- if you reach a cell during the process without unvisited neighboring cells, consider this a dead-end :
+ * 		- go back in the path until reaching a cell with an unvisited neighbor
+ * 		- then continue the process
+ * 	- the generation is done when the stack is emptied, meaning there is no cell with unvisited neighbors left
+ */
 public class DepthFirstSearchMaze extends Maze {
 
-	private Map<Cell, Boolean> alreadyVisited;
+	private Map<Cell, Boolean> cellsAlreadyVisited;
 
+	/**
+	 * DepthFirstSearchMaze class constructor
+	 * @param length the length of the maze
+	 * @param height the height of the maze
+	 */
 	public DepthFirstSearchMaze(int length, int height) {
 		super(length, height);
-		this.alreadyVisited = new HashMap<Cell, Boolean>();
+
+		// Mark all cells unvisited
+		this.cellsAlreadyVisited = new HashMap<>();
 		for (int i = 0; i < length; i++) {
 			for (int j = 0; j < height; j++) {
-				alreadyVisited.put(this.getCell(i, j), false);
+				cellsAlreadyVisited.put(this.getCell(i, j), false);
 			}
 		}
+		
 		this.generate();
 	}
 
 	/**
-	 * return the neighboring cells which are not already visited
+	 * Return the unvisited neighboring cells of a cell
 	 *
-	 * @param cell : the cell we want the neighboring cells
-	 * @return the neighboring cells of the cell "cell" which are not already visited
+	 * @param cell the cell whose neighbors we want
+	 * @return the neighboring cells that are not visited
 	 */
-
 	private List<Cell> neighboringCells(Cell cell) {
-		List<Cell> neighboring = new ArrayList<Cell>();
-		if (cell.getY() != 0) {//si la cellule est pas en haut, on ajoute l'index du dessus
-			//la cellule du dessus n'a pas déjà eté visité
-			//Cell cellFrontOf=this.cells[cell.getX()-1][cell.getY()];
+		List<Cell> neighboring = new ArrayList<>();
+
+		// Case for neighboring cell above
+		if (!isExternalWall(cell, WallOrientation.NORTH)) {
 			Cell cellFrontOf = this.getCell(cell.getX(), cell.getY() - 1);
-			if (!alreadyVisited.get(cellFrontOf)) {
+			if (!cellsAlreadyVisited.get(cellFrontOf)) {
 				neighboring.add(cellFrontOf);
 			}
 		}
-		if (cell.getY() != this.height - 1) {//si la cellule est pas en bas, on ajoute l'index du dessous
+
+		// Case for neighboring cell below
+		if (!isExternalWall(cell, WallOrientation.SOUTH)) {
 			Cell cellFrontOf = this.getCell(cell.getX(), cell.getY() + 1);
-			if (!alreadyVisited.get(cellFrontOf)) {
+			if (!cellsAlreadyVisited.get(cellFrontOf)) {
 				neighboring.add(cellFrontOf);
 			}
 		}
 
-		if (cell.getX() != 0) {//si la cellule est pas à gauche du tableau, on ajoute la cellule de gauche
+		// Case for neighboring cell to the left
+		if (!isExternalWall(cell, WallOrientation.WEST)) {
 			Cell cellFrontOf = this.getCell(cell.getX() - 1, cell.getY());
-			if (!alreadyVisited.get(cellFrontOf)) {
+			if (!cellsAlreadyVisited.get(cellFrontOf)) {
 				neighboring.add(cellFrontOf);
 			}
 		}
 
-		if (cell.getX() != this.length - 1) {//si la cellule est pas tout à droite du tableau, on ajoute la cellule de droite
+		// Case for neighboring cell to the right
+		if (!isExternalWall(cell, WallOrientation.EAST)) {
 			Cell cellFrontOf = this.getCell(cell.getX() + 1, cell.getY());
-			if (!alreadyVisited.get(cellFrontOf)) {
+			if (!cellsAlreadyVisited.get(cellFrontOf)) {
 				neighboring.add(cellFrontOf);
 			}
 		}
+
 		return neighboring;
 	}
 
+	/**
+	 * Generate the maze by following a path to open walls of unvisited cells using the Depth First Search algorithm
+	 */
 	protected void generate() {
+		// Choose a random cell to start
 		int x = new Random().nextInt(this.length);
 		int y = new Random().nextInt(this.height);
-		//je choisit aléatoirement la première cellule
-		Cell actualCell = this.getCell(x, y);
+		Cell currentCell = this.getCell(x, y);
 
-		//intialisation
-		Stack<Cell> areVisited = new Stack<Cell>();
-		List<Cell> neighboring = new ArrayList<Cell>();
+		/*
+		 * Create the stack for the maze generation.
+		 * Stacks up the visited cells until reaching a dead-end, then unstack cells until finding a cell that has unvisited neighbors.
+		 * When the stack is empty again, this means all cells have been visited.
+		 */
+		Stack<Cell> cellsInPath = new Stack<>();
 
-		//tant que je n'ai pas visité tout le labyrinthe
+		// Repeat until all cells are visited (i.e. stack is empty)
 		do {
-			alreadyVisited.put(actualCell, true);
-			neighboring = this.neighboringCells(actualCell);
-			if (!neighboring.isEmpty()) {
-				areVisited.push(actualCell);
-				Cell neighborCell = neighboring.get(new Random().nextInt(neighboring.size()));//on choisit une case aléatoirement parmi les cases voisines
+			cellsAlreadyVisited.put(currentCell, true); // Mark the current cell visited
+			List<Cell> neighboringCells = this.neighboringCells(currentCell); // Get neighboring cells
+
+			if (!neighboringCells.isEmpty()) {
+				cellsInPath.push(currentCell); // Stack the current cell
+				Cell neighborCell = neighboringCells.get(new Random().nextInt(neighboringCells.size())); // Choose a random cell among neighboring cells
+
+				// Remove the wall between that cell and the current cell
 				try {
-					this.removeWall(actualCell, neighborCell);
+					this.removeWall(currentCell, neighborCell);
 				} catch (InvalidAdjacentCellException e) {
 					System.out.println("Error : Trying to open wall between two non-adjacent cells");
 				}
-				actualCell = neighborCell;
+
+				// Update the current cell
+				currentCell = neighborCell;
 			} else {
-				actualCell = areVisited.pop(); //au dernire tour on regardera une pile vide donc actualCell vaudra null
+				currentCell = cellsInPath.pop(); // Go back in the path to the last visited cell
 			}
-		} while (!areVisited.isEmpty());
-
-
-		//on crée une hashmap avec comme clé la cellule et comme valeur un boolean qui nous dit si elle a déjà été visité ou non
-		//on créer une liste de cellule possible
-		//on choisit une cellule parmis la liste
-		//on la met dans la pile LIFO cellule visité et on met dans le hashmap qu'elle est visité
-		//on regarde les cellules possible autour
-
-		//on choisit une cellule parmi celle possible
-		//on detruit le mur entre les 2
-
-		//si aucune cellule possible on remonte à la dernière cellule observé (dans notre magnifique pile toute bien faites)
-		//On le fait TANT QUE la pile n'est pas vide
+		} while (!cellsInPath.isEmpty());
 	}
 }
