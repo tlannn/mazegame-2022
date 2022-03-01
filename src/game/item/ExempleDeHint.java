bdf893dff8a6f69fa76a6f960062f366ef7c3a4a
Emma Everaere <emma.everaere2.etu@univ-lille.fr>
2022-03-01 09:56:04 +0100

    add test de comment gérer Hint et add itemPositionHint

---
+++ src/game/item/ExempleDeHint.java
@@ -0,0 +1,19 @@
+public class CoordinateHint extends FixedHint(cell: winningCell, ):
+    liste=["la winning cell se trouve à l'abcisse "+winningCell.abcisse(), ];
+
+
+
+
+
+CoordinateHint indice = new CoordinateHint(winningCell)
+indice.toString()
+
+
+Game {
+    listeIndices = []
+
+    constructeur() {
+        CoordinateHint indice = new CoordinateHint(winningCell, abscisse)
+        ItemPositionHint indice2 = new ItemPositionHint(position, item)
+    }
+}
---
+++ src/game/item/itemPositionHint.java
@@ -0,0 +1,9 @@
+public class ItemPositionHint extends FixedHint{
+    private positionX;
+    private positionY;
+    private item;
+
+    public ItemPositionHint(Item item){
+
+    }
+}
