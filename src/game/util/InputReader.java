e78ab848bd0b14d501db4f51ee894b980d643643
Theo Lannoy <theo.lannoy2.etu@univ-lille.fr>
2022-03-01 16:45:53 +0100

    Add InputReader class to get input from the user

---
+++ src/game/util/InputReader.java
@@ -0,0 +1,8 @@
+package game.util;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class InputReader {
+	public static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+}
