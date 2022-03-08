303f8c339e95914c61dad30898b0e48c39172d10
Theo Lannoy <theo.lannoy2.etu@univ-lille.fr>
2022-03-08 09:32:47 +0100

    Add class QuestCondition and subclasses

---
+++ src/game/quest/EarnGoldCondition.java
@@ -0,0 +1,17 @@
+package game.quest;
+
+import game.character.Player;
+
+public class EarnGoldCondition extends QuestCondition {
+	private Player player;
+	private final int goldRequired;
+
+	public EarnGoldCondition(Player player, int goldRequired) {
+		this.player = player;
+		this.goldRequired = goldRequired;
+	}
+
+	public boolean isCompleted() {
+		return this.player.getGold() >= this.goldRequired;
+	}
+}
---
+++ src/game/quest/QuestCondition.java
@@ -0,0 +1,7 @@
+package game.quest;
+
+public abstract class QuestCondition {
+	public QuestCondition() {}
+
+	public abstract boolean isCompleted();
+}
