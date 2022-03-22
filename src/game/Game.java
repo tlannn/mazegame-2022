03cce564f2f76d22b68c5d9c6259300c08b2ec6a
Damien Michelot <damienmichelot@Dams.local>
2022-03-22 09:40:01 +0100

    add method move() and move_orientation on Class Game

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,5 +1,12 @@
+package game;
+
+import java.util.*;
+import game.maze.*;
+import game.character.*;
+
 public class Game{
-    pulic game(){
+    
+    public game(){
 
     }
 
@@ -10,11 +17,33 @@ public class Game{
         System.out.println("Pour regarder autour de vous appuyer sur r ");
         if text.equals("a"){
             System.out.println("appuyer sur z pour avancer vers le nord, sur d pour aller vers l'est, sur q pour aller vers l'ouest sur s pour aller vers le sud");
-            this.move()
+            this.move();
         }
     }
 
-    public boolean move(Character player){
+    public void move_orientation(Orientation orientation, Maze maze, Character character){
+        int x = character.currentCell.getX();
+        int y = character.currentCell.getY();
+        if (orientation == Orientation.EAST){
+            character.currentCell = maze.getCell(x+1, y);
+        }
+        if (orientation == Orientation.WEST){
+            character.currentCell = maze.getCell(x-1, y);
+        }
+        if (orientation == Orientation.NORTH){
+            character.currentCell = maze.getCell(x, y-1);
+        }
+        if (orientation == Orientation.SOUTH){
+            character.currentCell = maze.getCell(x, y+1);
+        }
+    }
+
+    public boolean move(Orientation orientation, Maze maze, Character character){
+        if (character.movable && character.currentCell.possibleOrientations().contains(orientation) ){
+            this.move_orientation(orientation, maze, character);
+            return true;
+        }
+        else{return false;}
 
     }
 }
--- src/game/character/Character.java
+++ src/game/character/Character.java
@@ -20,29 +20,6 @@ public abstract class Character {
         return this.currentCell;
     }
 
-    public void move_orientation(Orientation orientation){
-        int x = this.currentCell.getX();
-        int y = this.currentCell.getY();
-        if (orientation == Orientation.EAST){
-            this.currentCell = new Cell(x+1, y);
-        }
-        if (orientation == Orientation.WEST){
-            this.currentCell = new Cell(x-1, y);
-        }
-        if (orientation == Orientation.NORTH){
-            this.currentCell = new Cell(x, y-1);
-        }
-        if (orientation == Orientation.SOUTH){
-            this.currentCell = new Cell(x, y+1);
-        }
-    }
-
-    public void move(Orientation orientation){
-        if (this.movable && this.currentCell.possibleOrientations().contains(orientation) ){
-            this.move_orientation(orientation);
-        }
-    }
-
     public String toString(){
         return this.name;
     }
