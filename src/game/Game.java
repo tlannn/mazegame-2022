523b5a97552d65cf4f89cc41e70292441208e222
Emma Everaere <emma.everaere2.etu@univ-lille.fr>
2022-03-22 10:07:32 +0100

    merge

--- src/game/Game.java
+++ src/game/Game.java
@@ -5,19 +5,38 @@ import game.maze.*;
 import game.character.Character;
 
 public class Game{
-    
+
     public Game(){
 
     }
 
-    public boolean playTurn(Character player){
+    public boolean playTurn(Player player){
         Scanner scan= new Scanner(System.in);
         String text= scan.nextLine();
         System.out.println("Pour avancer d'une case appuyé sur a ");
         System.out.println("Pour regarder autour de vous appuyer sur r ");
+        System.out.println("Pour utiliser un objet de votre inventaire appuyer sur u ");
+
         if text.equals("a"){
             System.out.println("appuyer sur z pour avancer vers le nord, sur d pour aller vers l'est, sur q pour aller vers l'ouest sur s pour aller vers le sud");
             this.move();
+        }
+
+        if text.equals("r"){
+            player.look();
+        }
+        if text.equals("u"){
+            //faire une boucle tant que l'utilisateur a pas envoyé le bon num et faire un truc retour pour si finalement il ne veut plus utiliser d'objet
+            System.out.println("Quel objet voulez vous utiliser ? (entrer son indice)");
+            int num=scan.nextInt();
+            if (num>=0 && num<player.getInventoryItems().size()){
+
+            }
+            else{}
+
+
+
+
         }
     }
 
@@ -54,5 +73,11 @@ public class Game{
         }
         else{return false;}
 
+    public String inventoryToString(Player player){
+        String res="";
+        int i=0;
+        for (Item item : player.getInventoryItems()){
+            res+=i+"-"+item.toString();
+        }
     }
 }
