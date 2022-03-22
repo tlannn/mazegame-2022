39d2a4da417c2bf3283f282ab80454fd4a42a25b
Damien Michelot <damienmichelot@Dams.local>
2022-03-22 09:03:59 +0100

    init Class Enigma

--- src/game/enigma/Enigma.java
+++ src/game/enigma/Enigma.java
@@ -1,5 +1,18 @@
 package game.enigma;
 
 public class Enigma {
+
+    private String statement;
+    private String[] answer;
+    private int correctAnswerIndex;
+
+    public Enigma(String statement, String[] answer, int correctAnswerIndex){
+
+        this.statement = statement;
+        this.answer = answer;
+        this.correctAnswerIndex = correctAnswerIndex;
+
+    }
+
     
 }
