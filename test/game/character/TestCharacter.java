19fce0f70d3a2608aae00fed6ec3369b002566b7
Damien Michelot <damienmichelot@Dams.local>
2022-03-27 21:59:35 +0200

    update TestCharacter

--- test/game/character/TestCharacter.java
+++ test/game/character/TestCharacter.java
@@ -3,23 +3,31 @@ package game.character;
 import org.junit.*;
 import static org.junit.Assert.*;
 
+import java.beans.Transient;
+
 import game.character.*;
 import game.character.Character;
 import game.item.*;
 import game.maze.*;
 
 public class TestCharacter{
+    
+    @Test
+    public void testMoveOrianttaion(){
+        Player player = new Player("Damien", cellStart);
+        assertEquals(player.getCurrentCell(), cellStart);
+        player.move(nextCell);
+        assertEquals(player.getCurrentCell(), nextCell);
+    }
 
     @Test
     public void testMove(){
-        Cell cellStart = new Cell(0,0);
         Player player = new Player("Damien", cellStart);
         Cell nextCell = new Cell(0,1);
         assertEquals(player.getCurrentCell(), cellStart);
         player.move(nextCell);
         assertEquals(player.getCurrentCell(), nextCell);
     }
-
 
     public static junit.framework.Test suite() {
         return new junit.framework.JUnit4TestAdapter(TestCharacter.class);
