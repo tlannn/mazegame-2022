c7b02b39dfe8dd0269af0282f16b7d46d1ebbf98
Theo Lannoy <theo.lannoy2.etu@univ-lille.fr>
2022-03-29 08:12:04 +0200

    Modify parchment management for Trader
    Remove parchments argument from constructor and add method to add parchments

--- src/game/character/Trader.java
+++ src/game/character/Trader.java
@@ -4,22 +4,29 @@ import game.item.Parchment;
 import game.maze.Cell;
 import game.util.InputReader;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Trader extends NonPlayerCharacter {
 	private int parchmentCost;
-	private Parchment[] parchments;
+	private List<Parchment> parchments;
 
 	/**
 	 * Class constructor
 	 */
-	public Trader(Parchment[] parchments, Cell startingCell) {
+	public Trader(Cell startingCell) {
 		super("Marchand", startingCell);
 
-		this.parchments = parchments;
+		this.parchments = new ArrayList<>();
 		this.parchmentCost = 5;
 	}
 
 	public void talk() {
 		// TODO
+	}
+
+	public void addParchment(Parchment parchment) {
+		this.parchments.add(parchment);
 	}
 
 	private void increaseParchmentCost() {
