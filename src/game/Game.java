a2c3d2e5352ff916d7a99c453667787302d05a67
Emma Everaere <emma.everaere2.etu@univ-lille.fr>
2022-03-29 10:03:27 +0200

    add game with a lot of error

--- src/game/Game.java
+++ src/game/Game.java
@@ -4,6 +4,10 @@ import java.util.*;
 import game.maze.*;
 import game.character.*;
 import game.character.Character;
+import game.character.*;
+import game.item.*;
+
+
 
 public class Game{
 
@@ -18,49 +22,25 @@ public class Game{
     }
 
     public boolean playTurn(Player player, Maze maze){
-      boolean bonText=false
-      while (! bonText){//ATTENTION actuellement je fais une boucle infini
-        player.look();
-        Scanner scan= new Scanner(System.in);
-        String text= scan.nextLine();
-        System.out.println("Pour avancer d'une case appuyé sur a ");
-        System.out.println("Pour regarder autour de vous appuyer sur r ");
-        System.out.println("Pour utiliser un objet de votre inventaire appuyer sur u ");
-        System.out.println("Pour parler avec un personnage appuyer sur p ");
-        System.out.println("Pour ramasser un objet appuyer sur t ");
-
-
-
-        if text.equals("a"){
-          boolean bonAvancement=false;
-          while(! bonAvancement){
-
-            System.out.println("appuyer sur z pour avancer vers le nord, sur d pour aller vers l'est, sur q pour aller vers l'ouest sur s pour aller vers le sud");
+        boolean bonText=false;
+        while (! bonText){//ATTENTION actuellement je fais une boucle infini
+            player.look();
+            Scanner scan= new Scanner(System.in);
             String text= scan.nextLine();
+            System.out.println("Pour avancer d'une case appuyé sur a ");
+            System.out.println("Pour regarder autour de vous appuyer sur r ");
+            System.out.println("Pour utiliser un objet de votre inventaire appuyer sur u ");
+            System.out.println("Pour parler avec un personnage appuyer sur p ");
+            System.out.println("Pour ramasser un objet appuyer sur t ");
 
-            if(text.equals("z")){
-              bonAvancement=this.move(Orientation.NORTH, maze, player);
-            }
-            else if(text.equals("d")){
-              bonAvancement=this.move(Orientation.EAST, maze, player);
-            }
-            else if(text.equals("q")){
-              bonAvancement=this.move(Orientation.WEAST, maze, player);
-            }
-            else if(text.equals("s")){
-              bonAvancement=this.move(Orientation.SOUTH, maze, player);
-            }
-            else if (text.equals("a")){
-              bonAvancement=true;
-            }
-            if{ bonAvancement==false){
-              system.out.println("Ce déplacement n'est pas possible, taper 'a' pour quitter");
-            }
-          }
-        }
+            if (text.equals("a")){
+                boolean bonAvancement=false;
+                while(! bonAvancement){
 
-        if text.equals("r"){//regarde les déplacements possibles
+                    System.out.println("appuyer sur z pour avancer vers le nord, sur d pour aller vers l'est, sur q pour aller vers l'ouest sur s pour aller vers le sud");
+                    String text= scan.nextLine();
 
+<<<<<<< HEAD
         }
         if text.equals("u"){
             //faire une boucle tant que l'utilisateur a pas envoyé le bon num et faire un truc retour pour si finalement il ne veut plus utiliser d'objet
@@ -79,11 +59,111 @@ public class Game{
               else{
                 System.out.println("Ce numero n'est pas valide, si vous ne voulez pas utiliser d'objet appyer sur q");
               }
+=======
+                    if(text.equals("z")){
+                        bonAvancement=this.move(Orientation.NORTH, maze, player);
+                    }
+                    else if(text.equals("d")){
+                        bonAvancement=this.move(Orientation.EAST, maze, player);
+                    }
+                    else if(text.equals("q")){
+                        bonAvancement=this.move(Orientation.WEST, maze, player);
+                    }
+                    else if(text.equals("s")){
+                        bonAvancement=this.move(Orientation.SOUTH, maze, player);
+                    }
+                    else if (text.equals("a")){
+                        bonAvancement=true;
+                    }
+                    if(bonAvancement==false){
+                        system.out.println("Ce déplacement n'est pas possible, taper 'a' pour quitter");
+                    }
+                }
             }
-          }
+
+            if (text.equals("r")){//regarde les déplacements possibles
+                List <Orientation> orientationPossible = player.getCurrentCell().possibleOrientations();
+                System.out.println("Vous pouvez aller au:");
+                switch (orientation){
+                    case Orientation.NORTH:
+                    System.out.println("nord");
+                    break;
+                    case Orientation.SOUTH:
+                    System.out.println("sud");
+                    break;
+                    case Orientation.EAST:
+                    System.out.println("est");
+                    break;
+                    case Orientation.WEST:
+                    System.out.println("ouest");
+                    break;
+                }
+            }
+            if (text.equals("u")){
+                //faire une boucle tant que l'utilisateur a pas envoyé le bon num et faire un truc retour pour si finalement il ne veut plus utiliser d'objet
+                boolean bonNum=false;
+                while (!bonNum){
+                    System.out.println("Quel objet voulez vous utiliser ? (entrer son indice)");
+                    System.out.println(inventoryToString());
+                    int num=scan.nextInt();
+                    if (num>=0 && num<player.getInventoryItems().size()){
+                        bonNum=true;
+                        player.getInventoryItems().get(num).use();//on utilise l'item
+                    }
+                    else if(num.equals("q")){
+                        bonNum=true;
+                    }
+                    else{
+                        System.out.println("Ce numero n'est pas valide, si vous ne voulez pas utiliser d'objet appuyer sur q");
+                    }
+                }
+            }
+
+            if (text.equals("t")){
+                List <Item> items= player.getCurrentCell().getItemsInCell();
+                boolean bonNum=false;
+                while(!bonNum){
+                    System.out.println("quel item voulez vous prendre ? (taper son indice)");
+                    for (int i=0; i<items.size();i++){
+                        System.out.print(i+"-"+items.get(i)+" ");
+                    }
+                    int num=scan.nextInt();
+                    if (num>=0 && num<items.size()){
+                        bonNum=true;
+                        player.addItem(items.get(num));
+                    }
+                    else if(num.equals("q")){
+                        bonNum=true;
+                    }
+                    else{
+                        System.out.println("Ce numero n'est pas valide, si vous ne voulez pas ramasser d'objet appuyer sur q");
+                    }
+                }
+            }
+            if (text.equals("p")){
+                List <NonPlayerCharacter> characters = player.getCurrentCell().getCharactersInCell();
+                boolean bonNum=false;
+                while(!bonNum){
+                    System.out.println("A qui voulez vous parler ? (taper son indice)");
+                    for (int i=0; i<characters.size();i++){
+                        System.out.print(i+"-"+characters.get(i)+" ");
+                    }
+                    int num=scan.nextInt();
+                    if (num>=0 && num<characters.size()){
+                        bonNum=true;
+                        characters.get(num).talk();
+                    }
+                    else if(num.equals("q")){
+                        bonNum=true;
+                    }
+                    else{
+                        System.out.println("Ce numero n'est pas valide, si vous ne voulez pas parler avec un personnage appuyer sur q");
+                    }
+                }
+>>>>>>> c84d7dd (add game with a lot of error)
+            }
         }
     }
-
 
     public String inventoryToString(Player player){
         String res="";
