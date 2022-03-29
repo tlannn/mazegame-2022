582d68bbfa515dd5cd5b08ee6aacf7f026661830
Damien Michelot <damienmichelot@Dams.local>
2022-03-29 09:59:13 +0200

    update TestGame

--- test/game/TestGame.java
+++ test/game/TestGame.java
@@ -10,12 +10,18 @@ public class TestGame {
     public void testMoveOrientation() throws Exception{
         Maze maze = new DepthFirstSearchMaze(4, 5);
         Cell startcell = maze.getCell(2, 3);
+        Cell objectifCell = maze.getCell(2, 2);
+        startcell.setNorthWall(false);
         Player player = new Player("Damien", startcell);
-        Game game = new Game(maze, player);
-        List<Orientation> possibleOrientations = player.getCurrentCell().possibleOrientations();
+        Game game = new Game(maze, player);        
         assertTrue(player.getCurrentCell() == startcell);
-        game.move(possibleOrientations[0]);
+        game.move(Orientation.NORTH);
         assertFalse(player.getCurrentCell() == startcell);
+        assertEquals(player.getCurrecntCell(), objectifCell); 
+    }
+
+    public static junit.framework.Test suite() {
+        return new junit.framework.JUnit4TestAdapter(TestGame.class);
     }
 
 }
