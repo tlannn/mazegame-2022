d79aafeb293556456d44378933301b1fb30cdc76
Chergui Rayan <chergui@a10p5.fil.univ-lille1.fr>
2022-04-08 14:55:58 +0200

    Talk Trader

--- src/game/character/Trader.java
+++ src/game/character/Trader.java
@@ -6,6 +6,8 @@ import game.util.InputReader;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 
 public class Trader extends NonPlayerCharacter {
 	private int parchmentCost;
@@ -21,12 +23,25 @@ public class Trader extends NonPlayerCharacter {
 		this.parchmentCost = 5;
 	}
 
-	public void talk() {
-		// TODO
+	public void talk(Player player) {
+		System.out.println("En échange de la modique somme de " + this.parchmentCost + "galons d'or, souhaitez-vous acquérir ce parchemin ?");
+		Scanner scan= new Scanner(System.in);
+		String text= scan.nextLine();
+		if(text.equals("o") ){
+			player.getInventory().addItem(parchments.get(0));
+			this.removeParchment(parchments.get(0));
+			this.increaseParchmentCost();
+		}
+		scan.close();
 	}
 
 	public void addParchment(Parchment parchment) {
 		this.parchments.add(parchment);
+	}
+
+	private void removeParchment(Parchment parchment){
+		this.parchments.remove(parchment);
+
 	}
 
 	private void increaseParchmentCost() {
