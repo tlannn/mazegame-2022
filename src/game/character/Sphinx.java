4b901d899f5501dafc5a0d63edb02c5e047b7af6
Theo Lannoy <theo.lannoy2.etu@univ-lille.fr>
2022-04-30 19:01:16 +0200

    Change some text displayed

--- src/game/character/Sphinx.java
+++ src/game/character/Sphinx.java
@@ -41,29 +41,29 @@ public class Sphinx extends NonPlayerCharacter {
 
 	public void talk(GraphicsSystem graphicsSystem, InputSystem inputSystem, Player player) {
 		super.talk(graphicsSystem,inputSystem,player);
-		System.out.println("Bonjour, je m'appelle Léo le sphinx");
+		System.out.println("Bonjour, je m'appelle Léo le Sphinx.");
 		if (!this.hasGivenHint) {
 			int i = 0;
-			System.out.println("je ne t'ai pas encore donné d'indice...");
+			System.out.println("Il semblerait que je ne t'ai pas encore donné d'indice...");
 
 			if(i < this.enigmas.size() && ! this.enigmas.get(i).getIsResolved()){
-				System.out.println("Voici mon enigme :");
+				System.out.println("Résout mon énigme, et mon indice sera tiens :");
 					System.out.println(this.enigmas.get(i).toString());
 					Scanner scan= new Scanner(System.in);
 					String text = scan.nextLine();
 					try{
 						this.enigmas.get(i).resolve(text);
             if(this.enigmas.get(i).getIsResolved()){
-							System.out.println("Bien joué, c'est la bonne réponse ! En récompense je te donne cet indice :");
+							System.out.println("Bien joué, c'est la bonne réponse ! En récompense, je te donne cet indice :");
 							System.out.println(this.hint.toString());
 							this.hasGivenHint = true;
 						}
 						else{
-							System.out.println("Désolé c'est la mauvaise réponse");
+							System.out.println("Navré aventurier, mais c'est la mauvaise réponse. Reviens me voir si tu veux retenter ta chance.");
 						}
           }
           catch(Exception AnswerNoContainsQCM) {
-              System.out.println("Cette réponse ne fait pas partit de celles proposées");
+              System.out.println("Cette réponse ne fait pas partie de celles proposées.");
           }
 					// quand je ferme le scan ça fait un bug
 					// scan.close();
