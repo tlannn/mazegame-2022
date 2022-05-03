59ad7668ce664d3179113403ef95724ef635ade5
Damien Michelot <damienmichelot@Dams.local>
2022-05-03 08:38:05 +0200

    ajout d'un sphinx en art ascii

--- src/game/character/dialog/EnigmaDialog.java
+++ src/game/character/dialog/EnigmaDialog.java
@@ -24,6 +24,16 @@ public class EnigmaDialog extends Dialog {
         GraphicsSystem graphics = Game.getGraphicsSystem();
 
         if (!this.sphinx.hasGivenHint()) {
+
+
+        System.out.println("\n\n       .~~~.");
+        System.out.println("      /|6 6|\\");
+        System.out.println("     /O\\_^_/O\\");
+        System.out.println("     \\/`===`\\/");
+        System.out.println("     ,| |^| |.");
+        System.out.println("____(n(n)_(n)n)____");
+        System.out.println("%%%%%%%%%%%%%%%%%%%\n\n");
+
             graphics.displayText("Bonjour," + SpeechPauseSystem.SLOW_PAUSE_DELAY_TAG + " je m'appelle Léo le Sphinx." + SpeechPauseSystem.LONG_PAUSE_DELAY_TAG);
             graphics.displayText("Il semblerait que je ne vous ai pas encore donné d'indice" + SpeechPauseSystem.SLOW_PAUSE_DELAY_TAG + "." + SpeechPauseSystem.SLOW_PAUSE_DELAY_TAG + "." + SpeechPauseSystem.SLOW_PAUSE_DELAY_TAG + "." + SpeechPauseSystem.LONG_PAUSE_DELAY_TAG);
             List<Enigma> enigmas = this.sphinx.getEnigmas();
