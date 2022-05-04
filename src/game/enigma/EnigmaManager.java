4f3d2e602405e01d2dea2b6d1646316b240be265
Theo Lannoy <theo.lannoy2.etu@univ-lille.fr>
2022-05-04 07:39:45 +0200

    Use iterator to look through enigmas in EnigmaManager

--- src/game/enigma/EnigmaManager.java
+++ src/game/enigma/EnigmaManager.java
@@ -2,33 +2,42 @@ package game.enigma;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class EnigmaManager {
     //private Map<Enigma, Boolean> enigmas;
     private List<Enigma> enigmas;
+    private ListIterator<Enigma> it;
 
     public EnigmaManager() {
         this.enigmas = new ArrayList<>();
+        this.it = enigmas.listIterator();
     }
 
     public EnigmaManager(List<Enigma> enigmas) {
         this();
         this.enigmas.addAll(enigmas);
+        this.it = enigmas.listIterator();
     }
 
     public void addEnigma(Enigma enigma) {
-        this.enigmas.add(enigma);
+        this.it.set(enigma);
+        this.it.previous();
     }
 
     public Enigma getEnigma() {
-        Enigma enigma = null;
+        int itemsLooked = 0;
 
-        for (Enigma e : this.enigmas) {
-            if (!e.isResolved()) {
-                enigma = e; break;
-            }
+        while (itemsLooked < this.enigmas.size()) { // Look through all enigmas
+            if (!this.it.hasNext()) this.it = this.enigmas.listIterator(); // Reset the iterator to the beginning if end is reached
+            Enigma enigma = this.it.next(); // Take next enigma
+            ++itemsLooked;
+
+            // When an unresolved enigma is reached, return it
+            if (!enigma.isResolved()) return enigma;
         }
 
-        return enigma;
+        // Reached if all enigmas are resolved
+        return null;
     }
 }
